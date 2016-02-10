dcc624c862d94bd0582f145299888574b0bd80b8
wmaterka <w.materka@gmail.com>
2016-02-10 13:12:38 +0100

    storyboard!

---
+++ VirtualTourist/Controllers/PhotoAlbumVC.swift
@@ -0,0 +1,23 @@
+//
+//  PhotoAlbumVC.swift
+//  VirtualTourist
+//
+//  Created by Wojtek Materka on 10/02/2016.
+//  Copyright © 2016 Wojtek Materka. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+
+class PhotoAlbumVC: UIViewController {
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var locationMap: MKMapView!
+    @IBOutlet weak var photoCollection: UICollectionView!
+    @IBOutlet weak var bottomButton: UIButton!
+    
+    
+}
---
+++ VirtualTourist/Controllers/TravelLocationsVC.swift
@@ -0,0 +1,26 @@
+//
+//  TravelLocationsVC.swift
+//  VirtualTourist
+//
+//  Created by Wojtek Materka on 10/02/2016.
+//  Copyright © 2016 Wojtek Materka. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class TravelLocationsVC: UIViewController {
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var travelMap: MKMapView!
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
